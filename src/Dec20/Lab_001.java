ef10189e1b646c4c424d844acb89a5419f8eb838
kalyan <kalyankundhala@gmail.com>
2023-12-25 13:52:24 +0530

    26th december 2023

--- src/Dec20/Lab_001.java
+++
@@ -1,7 +0,0 @@
-package src.Dec20;
-
-public class Lab_001 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Dec20/Lab_001_DataTypes.java
@@ -0,0 +1,29 @@
+package src.Dec20;
+
+public class Lab_001_DataTypes {
+    public static void main(String[] args) {
+
+        //Datatypes and variables
+        byte b= 20;
+        System.out.println(b);
+
+        short s= 47;
+        System.out.println(s);
+
+        int a= 123;
+        System.out.println(a);
+
+        long phno = 9876543210L;
+        System.out.println(phno);
+
+        float f= 12.34f;
+        System.out.println(f);
+
+        double d= 13.6545d;
+        System.out.println(d);
+
+        char c1 = 'A';
+        System.out.println(c1);
+
+    }
+}
